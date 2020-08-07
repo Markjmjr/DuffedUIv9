3e8c386b0369b4674c99459a633ac2ca9712ed4f
Christian Hann <c.hann@radiologie-saar.net>
2020-08-07 14:06:22 +0200

    Fixed bank and bags

--- DuffedUI/modules/loot/bags.lua
+++ DuffedUI/modules/loot/bags.lua
@@ -644,6 +644,7 @@ function Stuffing:BagFrameSlotNew(p, slot)
 		ret.slot = slot
 		slot = slot - 4
 		ret.frame = CreateFrame('ItemButton', 'StuffingBBag' .. slot .. 'Slot', p, 'BankItemButtonBagTemplate')
+		Mixin(ret.frame, BackdropTemplateMixin)
 		ret.frame:StripTextures()
 		ret.frame:SetID(slot)
 
@@ -675,6 +676,7 @@ function Stuffing:BagFrameSlotNew(p, slot)
 		end
 	else
 		ret.frame = CreateFrame('ItemButton', 'StuffingFBag' .. slot .. 'Slot', p, 'BagSlotButtonTemplate')
+		Mixin(ret.frame, BackdropTemplateMixin)
 
 		hooksecurefunc(ret.frame.IconBorder, 'SetVertexColor', function(self, r, g, b)
 			if r ~= 0.65882 and g ~= 0.65882 and b ~= 0.65882 then
