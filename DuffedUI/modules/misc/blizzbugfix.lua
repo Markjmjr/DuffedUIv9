5a64965375e7840e17205fdae10e8406814b7c91
liquidbase <liquid.base.x@gmail.com>
2020-08-13 21:22:21 +0200

    Fixed issue with NineSlice.lua

--- DuffedUI/modules/misc/blizzbugfix.lua
+++ DuffedUI/modules/misc/blizzbugfix.lua
@@ -1,5 +1,5 @@
 local D, C, L = unpack(select(2, ...))
-local Module = D:NewModule("BlizzBugFixes", "AceEvent-3.0", "AceHook-3.0")
+local Module = D:NewModule('BlizzBugFixes', 'AceEvent-3.0', 'AceHook-3.0')
 
 if not Module then
 	return
@@ -7,18 +7,18 @@ end
 
 -- Fix Blank Tooltip
 local bug = nil
-local FixTooltip = CreateFrame("Frame")
-FixTooltip:RegisterEvent("UPDATE_BONUS_ACTIONBAR")
-FixTooltip:RegisterEvent("ACTIONBAR_PAGE_CHANGED")
-FixTooltip:SetScript("OnEvent", function()
+local FixTooltip = CreateFrame('Frame')
+FixTooltip:RegisterEvent('UPDATE_BONUS_ACTIONBAR')
+FixTooltip:RegisterEvent('ACTIONBAR_PAGE_CHANGED')
+FixTooltip:SetScript('OnEvent', function()
 	if GameTooltip:IsShown() then
 		bug = true
 	end
 end)
 
-local FixTooltipBags = CreateFrame("Frame")
-FixTooltipBags:RegisterEvent("BAG_UPDATE_DELAYED")
-FixTooltipBags:SetScript("OnEvent", function()
+local FixTooltipBags = CreateFrame('Frame')
+FixTooltipBags:RegisterEvent('BAG_UPDATE_DELAYED')
+FixTooltipBags:SetScript('OnEvent', function()
 	if StuffingFrameBags and StuffingFrameBags:IsShown() then
 		if GameTooltip:IsShown() then
 			bug = true
@@ -26,7 +26,7 @@ FixTooltipBags:SetScript("OnEvent", function()
 	end
 end)
 
-GameTooltip:HookScript("OnTooltipCleared", function(self)
+GameTooltip:HookScript('OnTooltipCleared', function(self)
 	if self:IsForbidden() then
 		return
 	end
@@ -49,7 +49,7 @@ function Module:MisclickPopups()
 	if (PVPReadyDialog) then
 		PVPReadyDialog.leaveButton:Hide()
 		PVPReadyDialog.enterButton:ClearAllPoints()
-		PVPReadyDialog.enterButton:SetPoint("BOTTOM", PVPReadyDialog, "BOTTOM", 0, 25)
+		PVPReadyDialog.enterButton:SetPoint('BOTTOM', PVPReadyDialog, 'BOTTOM', 0, 25)
 	end
 end
 
@@ -64,8 +64,8 @@ function Module:BuyMaxStacks()
 			local name, _, quality, _, _, _, _, maxStack, _, texture = GetItemInfo(itemLink)
 			if maxStack and maxStack > 1 then
 				if not cache[itemLink] then
-					StaticPopupDialogs["BUY_STACK"] = {
-						text = "Stack Buying Check",
+					StaticPopupDialogs['BUY_STACK'] = {
+						text = 'Stack Buying Check',
 						button1 = YES,
 						button2 = NO,
 						OnAccept = function()
@@ -77,7 +77,7 @@ function Module:BuyMaxStacks()
 					}
 
 					local r, g, b = GetItemQualityColor(quality or 1)
-					StaticPopup_Show("BUY_STACK", " ", " ", {["texture"] = texture, ["name"] = name, ["color"] = {r, g, b, 1}, ["link"] = itemLink, ["index"] = id, ["count"] = maxStack})
+					StaticPopup_Show('BUY_STACK', ' ', ' ', {['texture'] = texture, ['name'] = name, ['color'] = {r, g, b, 1}, ['link'] = itemLink, ['index'] = id, ['count'] = maxStack})
 				else
 					BuyMerchantItem(id, GetMerchantItemMaxStack(id))
 				end
@@ -96,25 +96,52 @@ function Module:OnEnable()
 	self:BuyMaxStacks()
 	self:MisclickPopups()
 	
-	hooksecurefunc(StaticPopupDialogs["DELETE_GOOD_ITEM"], "OnShow", function(self)
+	hooksecurefunc(StaticPopupDialogs['DELETE_GOOD_ITEM'], 'OnShow', function(self)
 		self.editBox:SetText(DELETE_ITEM_CONFIRM_STRING)
 	end)
 
 	for i = 0, 3 do
-		local bagSlot = _G["CharacterBag"..i.."Slot"]
-		bagSlot:UnregisterEvent("ITEM_PUSH") -- Gets Rid Of The Animation
+		local bagSlot = _G['CharacterBag'..i..'Slot']
+		bagSlot:UnregisterEvent('ITEM_PUSH') -- Gets Rid Of The Animation
 	end
 
 	-- Make It Only Split Stacks With Shift-RightClick If The Tradeskillframe Is Open
 	-- Shift-LeftClick Should Be Reserved For The Search Box
 	local function hideSplitFrame(_, button)
 		if TradeSkillFrame and TradeSkillFrame:IsShown() then
-			if button == "LeftButton" then
+			if button == 'LeftButton' then
 				StackSplitFrame:Hide()
 			end
 		end
 	end
 
-	hooksecurefunc("ContainerFrameItemButton_OnModifiedClick", hideSplitFrame)
-	hooksecurefunc("MerchantItemButton_OnModifiedClick", hideSplitFrame)
+	hooksecurefunc('ContainerFrameItemButton_OnModifiedClick', hideSplitFrame)
+	hooksecurefunc('MerchantItemButton_OnModifiedClick', hideSplitFrame)
 end
+
+-- Fixes a bug in the Blizzard code that tainted self-created backdrops on SecureFrames.
+hooksecurefunc(NineSliceUtil, 'ApplyLayout', function(frame, layout)
+    if not rawget(frame, 'ApplyBackdrop') or rawget(layout, 'setupPieceVisualsFunction') ~= BackdropTemplateMixin.SetupPieceVisuals then
+        -- Probably not a backdrop being applied.
+        return
+    end
+
+    for name, piece in pairs(layout) do
+        if type(piece) == 'table' then
+            local key = next(piece)
+            while key do
+                if name ~= 'Center' or key ~= 'layer' then
+                    piece[key] = nil
+                end
+
+                key = next(piece, key)
+            end
+
+            local i = 0
+
+            while not issecurevariable(piece, 'mirrorLayout') do
+                i, piece['__detaint' .. i] = i + 1, nil
+            end
+        end
+    end
+end)
