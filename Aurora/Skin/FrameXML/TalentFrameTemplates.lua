200ddc07a83c682cae8684b99985aa7eb8f1d49c
liquidbase <liquid.base.x@gmail.com>
2020-08-15 13:54:07 +0200

    Fixed temporary Aurora

--- Aurora/Skin/FrameXML/TalentFrameTemplates.lua
+++ Aurora/Skin/FrameXML/TalentFrameTemplates.lua
@@ -12,16 +12,16 @@ local Color = Aurora.Color
 
 do --[[ FrameXML\TalentFrameTemplates.xml ]]
     function Skin.PvpTalentSlotTemplate(Button)
-        Button._auroraBG = Base.CropIcon(Button.Texture, Button)
+        --[[Button._auroraBG = Base.CropIcon(Button.Texture, Button)
         Button.Arrow:SetVertexColor(Color.yellow:GetRGB())
         Button.Arrow:SetPoint("LEFT", Button.Texture, "RIGHT", 5, 0)
         Button.Arrow:SetSize(26, 13)
         Base.SetTexture(Button.Arrow, "arrowRight")
-        Button.Border:Hide()
+        Button.Border:Hide()]]--
     end
     function Skin.PvpTalentTrinketSlotTemplate(Button)
-        Skin.PvpTalentSlotTemplate(Button)
-        Button.Texture:SetSize(48, 48)
+        --Skin.PvpTalentSlotTemplate(Button)
+        --Button.Texture:SetSize(48, 48)
     end
 end
 
