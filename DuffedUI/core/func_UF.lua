79ee8ba5219b96167553d39d206f260e61db2e14
Christian Hann <c.hann@radiologie-saar.net>
2020-08-17 14:34:10 +0200

    Fixed buff tracking on raidframes

--- DuffedUI/core/func_UF.lua
+++ DuffedUI/core/func_UF.lua
@@ -536,7 +536,7 @@ local function Defaults(priorityOverride)
 end
 
 -- BuffWatch: List of personal spells to show on unitframes as icon
-local function AuraWatch_AddSpell(id, point, color, anyUnit, onlyShowMissing, displayText, textThreshold, xOffset, yOffset, sizeOverride)
+D['AuraWatch_AddSpell'] = function(id, point, color, anyUnit, onlyShowMissing, displayText, textThreshold, xOffset, yOffset, sizeOverride)
 
 	local r, g, b = 1, 1, 1
 	if color then r, g, b = unpack(color) end
