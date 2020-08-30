2fee055a4b74a6a437c4c33cd83adc4955a271d8
liquidbase <liquid.base.x@gmail.com>
2020-08-30 10:18:56 +0200

    Added correct visual style to chatbubbles via SetTemplate and localized config values

--- DuffedUI/modules/chat/chatbubbles.lua
+++ DuffedUI/modules/chat/chatbubbles.lua
@@ -7,6 +7,8 @@ local _G = _G
 local CreateFrame = _G.CreateFrame
 local GetCVarBool = _G.GetCVarBool
 local C_ChatBubbles_GetAllChatBubbles = _G.C_ChatBubbles.GetAllChatBubbles
+local font = C['media']['font']
+local fontsize = C['chat']['chatbubblesfontsize']
 
 local function getBackdrop()
 	return {
@@ -31,12 +33,10 @@ local function ReskinChatBubble(chatbubble)
 		bg:SetScale(UIParent:GetEffectiveScale())
 		bg:SetInside(frame, 6, 6)
 
-		bg:SetFrameStrata("BACKGROUND")
-		frame:SetBackdrop(getBackdrop())
-		frame:SetBackdropColor(C['general']['backdropcolor'][1], C['general']['backdropcolor'][2], C['general']['backdropcolor'][3], C['general']['backdropcolor'][4])
-		frame:SetBackdropBorderColor(C['general']['bordercolor'][1], C['general']['bordercolor'][2], C['general']['bordercolor'][3], C['general']['bordercolor'][4])
+		bg:SetFrameStrata('BACKGROUND')
+		frame:SetTemplate('Transparent')
 		frame.Tail:SetAlpha(0)
-		frame.String:SetFont(C['media']['font'], C['chat']['chatbubblesfontsize'], "OUTLINE")
+		frame.String:SetFont(font, fontsize, 'OUTLINE')
 	end
 
 	chatbubble.styled = true
@@ -44,29 +44,29 @@ end
 
 function Module:CreateChatBubbles()
 	local events = {
-		CHAT_MSG_SAY = "chatBubbles",
-		CHAT_MSG_YELL = "chatBubbles",
-		CHAT_MSG_MONSTER_SAY = "chatBubbles",
-		CHAT_MSG_MONSTER_YELL = "chatBubbles",
-		CHAT_MSG_PARTY = "chatBubblesParty",
-		CHAT_MSG_PARTY_LEADER = "chatBubblesParty",
-		CHAT_MSG_MONSTER_PARTY = "chatBubblesParty",
+		CHAT_MSG_SAY = 'chatBubbles',
+		CHAT_MSG_YELL = 'chatBubbles',
+		CHAT_MSG_MONSTER_SAY = 'chatBubbles',
+		CHAT_MSG_MONSTER_YELL = 'chatBubbles',
+		CHAT_MSG_PARTY = 'chatBubblesParty',
+		CHAT_MSG_PARTY_LEADER = 'chatBubblesParty',
+		CHAT_MSG_MONSTER_PARTY = 'chatBubblesParty',
 	}
 
-	local bubbleHook = CreateFrame("Frame")
+	local bubbleHook = CreateFrame('Frame')
 
 	for event in next, events do
 		bubbleHook:RegisterEvent(event)
 	end
 
-	bubbleHook:SetScript("OnEvent", function(self, event, msg)
+	bubbleHook:SetScript('OnEvent', function(self, event, msg)
 		if GetCVarBool(events[event]) then
 			self.elapsed = 0
 			self:Show()
 		end
 	end)
 
-	bubbleHook:SetScript("OnUpdate", function(self, elapsed)
+	bubbleHook:SetScript('OnUpdate', function(self, elapsed)
 		self.elapsed = self.elapsed + elapsed
 		if self.elapsed > .1 then
 			for _, chatbubble in pairs(C_ChatBubbles_GetAllChatBubbles()) do
